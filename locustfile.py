5f3e5a377778d4ec55653bfc6cd6e61c70c433b3
Trent Smith <tsmith12@ucsc.edu>
2018-05-10 00:28:48 -0700

    Adding host

--- locustfile.py
+++ locustfile.py
@@ -1,14 +1,18 @@
+import os
 from locust import HttpLocust
 
 # from locustfiles.common.dsslocust import DSSLocust
 # from locustfiles import UploadTaskSet, NotifyTaskSet, SearchTaskSet, DownloadTaskSet, CheckoutTaskSet
 from locustfiles import SearchTaskSet
 
+HOST = os.getenv('TARGET_URL', "http://localhost")
+
 # class UploadUser(DSSLocust):
 #     min_wait = 1000
 #     max_wait = 3000
 #     task_set = UploadTaskSet
 #     weight = 1
+#     host = HOST
 #
 #
 # class CheckoutUser(DSSLocust):
@@ -16,6 +20,7 @@ from locustfiles import SearchTaskSet
 #     max_wait = 3000
 #     task_set = CheckoutTaskSet
 #     weight = 3
+#     host = HOST
 #
 #
 # class DownloadUser(DSSLocust):
@@ -23,6 +28,7 @@ from locustfiles import SearchTaskSet
 #     max_wait = 1000
 #     task_set = DownloadTaskSet
 #     weight = 3
+#     host = HOST
 #
 #
 # class NotifiedUser(DSSLocust):
@@ -30,6 +36,7 @@ from locustfiles import SearchTaskSet
 #     max_wait = 3000
 #     task_set = NotifyTaskSet
 #     weight = 2
+#     host = HOST
 
 
 class SearchUser(HttpLocust):
@@ -37,3 +44,4 @@ class SearchUser(HttpLocust):
     max_wait = 500
     task_set = SearchTaskSet
     weight = 4
+    host = HOST
